e962d4016775c901afbb23a10f4d28df88109e7a
Sachin Prasad <147019955+Sachinpd-1703@users.noreply.github.com>
2025-10-16 17:15:36 +0530

    google_auth file created

---
+++ google_auth.py
@@ -0,0 +1,11 @@
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = [
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/drive.metadata.readonly"
+]
